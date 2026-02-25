d1f5ccf6f8550883b6ccf78ab570ba95df5d5120
pes10k <pes@peteresnyder.com>
2026-02-25 16:41:31 -0600

    remove errant linting exceptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
