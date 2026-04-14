a7c9df5664c0c8af02005a36402de402854c789b
pes10k <pes@peteresnyder.com>
2026-04-14 09:42:56 -0500

    remove orphaned "dist" files

--- dist/bot-detection.js
+++
@@ -1,9 +0,0 @@
-const injected_botDetectEvasion = () => {
-    if (window.navigator.webdriver) {
-        Object.defineProperty(navigator, "webdriver", { get: () => undefined });
-    }
-};
-export const applyBotDetectEvasion = async (logger, context) => {
-    await context.addInitScript(injected_botDetectEvasion);
-};
-//# sourceMappingURL=bot-detection.js.map
